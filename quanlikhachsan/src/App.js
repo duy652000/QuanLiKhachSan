cc91d7aa4f0392d0742b497beb0a1468e8593842
duy652000 <94160086+duy652000@users.noreply.github.com>
2022-10-05 23:57:55 +0700

    v1
    - code login

--- quanlikhachsan/src/App.js
+++ quanlikhachsan/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import LoginForm from "./components/login/LoginForm";
 
 function App() {
+  const adminUser = {
+    name: "duy652000",
+    password: "123123",
+  };
+
+  const [user, setUser] = useState({ name: "" });
+  const [error, setError] = useState("");
+
+  const Login = (details) => {
+    if (
+      details.name == adminUser.name &&
+      details.password == adminUser.password
+    ) {
+      setUser({ name: details.name });
+    } else {
+      setError("Sai thông tin đăng nhập , vui lòng đăng nhập lại !");
+    }
+  };
+
+  const Logout = (e) => {
+    setUser({ name: "" });
+    setError("");
+    e.preventDefault();
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user.name != "" ? (
+        <div className="wellcome">
+          <h2>
+            Wellcome ,<span>{user.name}</span>
+          </h2>
+          <button onClick={Logout}>Logout</button>
+        </div>
+      ) : (
+        <LoginForm Login={Login} error={error} />
+      )}
     </div>
   );
 }
---
+++ quanlikhachsan/src/components/login/LoginForm.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+function LoginForm({ Login, error }) {
+  const [details, setDetails] = useState({ name: "", password: "" });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    Login(details);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-inner">
+        <h2>Login</h2>
+        {error != "" ? <div className="error-login">{error}</div> : ""}
+        <br></br>
+        <div className="form-group">
+          <label htmlFor="name">Name </label>
+          <input
+            type="name"
+            name="name"
+            id="name"
+            onChange={(e) => setDetails({ ...details, name: e.target.value })}
+            value={details.name}
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={(e) =>
+              setDetails({ ...details, password: e.target.value })
+            }
+            value={details.password}
+          />
+        </div>
+
+        <button type="submit">Đăng Nhập</button>
+      </div>
+    </form>
+  );
+}
+
+export default LoginForm;
