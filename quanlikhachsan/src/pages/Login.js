45dd468f6f9f3fa646054f5a37b3f6b676fb3b40
duy652000 <94160086+duy652000@users.noreply.github.com>
2022-10-09 16:39:42 +0700

    v8
    - login

--- quanlikhachsan/src/pages/Login.js
+++ quanlikhachsan/src/pages/Login.js
@@ -1,92 +1,90 @@
 import React, { useEffect, useState } from "react";
-import { MDBContainer, MDBInput, MDBCheckbox } from "mdb-react-ui-kit";
+import { MDBContainer, MDBInput } from "mdb-react-ui-kit";
 import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const history = useNavigate();
   useEffect(() => {
     if (localStorage.getItem("token")) {
       history.push("*");
     }
-  },[]);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const history = useNavigate();
-  
+  });
+  const [details, setDetails] = useState({ email: "", password: "" });
+  const handleLogin = (e) => {
+    e.preventDefault();
+    Login(details);
+    // console.log(details);
+  };
+  async function Login(detail) {
+    const formData = new FormData();
+    formData.append("email", detail.email);
+    formData.append("password", detail.password);
 
+    let data = detail;
+    //
+    try {
+      let result = await fetch("http://127.0.0.1:8000/login", {
+        method: "POST",
+        mode: "no-cors",
+        headers: {
+          Accept: "/",
+        },
+        body: formData,
+      });
+      result = await result.json();
+      console.log(result);
+    } catch (error) {
+      console.warn(error);
+    }
+    //
 
- 
+    // localStorage.setItem("token", JSON.stringify(result));
 
-
-  async function handleLogin() {
-    
-    
-    
-    let item = { email,password };
-    // console.log(email, password);
-
-    let result = await fetch(`http://127.0.0.1:3000/login`, {
-      method: "POST",
-      mode: 'no-cors',
-
-      body: JSON.stringify(item),
-      headers: {
-        "Content-type": "application/json",
-        "Accept": "application/json",
-      },
-    });
-
-    result = await result.json();
-    localStorage.setItem("token", JSON.stringify(result));
-
-    history.push("/");
-  
+    // history("/");
   }
 
-  
   return (
-    <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
-      <MDBInput
-        wrapperClass="mb-4"
-        placeholder="Email address"
-        id="form1"
-        type="email"
-        onChange={(e) => {
-          setEmail(e.target.value);
-        }}
-        value={email}
-      />
-      <MDBInput
-        wrapperClass="mb-4"
-        placeholder="Password"
-        id="form2"
-        type="password"
-        onChange={(e) => {
-          setPassword(e.target.value);
-        }}
-        value={password}
-      />
+    <form onSubmit={handleLogin}>
+      <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
+        <div className="form-group">
+          <MDBInput
+            wrapperClass="mb-4"
+            placeholder="Email address"
+            id="email"
+            name="email"
+            type="email"
+            onChange={(e) => {
+              setDetails({ ...details, email: e.target.value });
+            }}
+            value={details.email}
+          />
+        </div>
+        <div className="form-group">
+          <MDBInput
+            wrapperClass="mb-4"
+            placeholder="Password"
+            id="password"
+            name="password"
+            type="password"
+            onChange={(e) =>
+              setDetails({ ...details, password: e.target.value })
+            }
+            value={details.password}
+          />
+        </div>
 
-      <div className="d-flex justify-content-between mx-3 mb-4">
-        <MDBCheckbox
-          name="flexCheck"
-          value=""
-          id="flexCheckDefault"
-          label="Remember me"
-        />
-        <a href="#">Forgot password?</a>
-      </div>
+        <button type="submit" className="mb-4 btn btn-primary">
+          Sign in
+        </button>
 
-      <button onClick={handleLogin} className="mb-4 btn btn-primary">
-        Sign in
-      </button>
-
-      <div className="text-center">
-        <div
-          className="d-flex justify-content-between mx-auto"
-          style={{ width: "40%" }}
-        ></div>
-      </div>
-    </MDBContainer>
+        <div className="text-center">
+          <div
+            className="d-flex justify-content-between mx-auto"
+            style={{ width: "40%" }}
+          ></div>
+        </div>
+      </MDBContainer>
+    </form>
   );
 }
 
