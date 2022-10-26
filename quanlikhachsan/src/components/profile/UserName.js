80c4519901a8ec448baef50ee772f038d69570b1
duy652000 <94160086+duy652000@users.noreply.github.com>
2022-10-26 23:31:52 +0700

    v 20
    - fix username

--- quanlikhachsan/src/components/profile/UserName.js
+++ quanlikhachsan/src/components/profile/UserName.js
@@ -4,12 +4,13 @@ import React from "react";
  
 
 function userName(){
-
+if(localStorage.getItem("token")){
   const token = localStorage.getItem("token");
   const decoded = jwtDecode(token);
   let username = decoded[0];
   return username;
-
+}
+return "" ;
 }
 
   
@@ -18,9 +19,10 @@ function UserName() {
   return (
     <span className="mr-2 d-none d-lg-inline text-gray-600 small">
      {/* {username?username.toUpperCase():""} */}
-     {userName()}
+     {userName().toUpperCase()}
     </span>
   );
 }
+
 
 export default UserName;
