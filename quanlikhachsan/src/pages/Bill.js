415df30fa4337cee2ff12172423f5f307a80ff09
duy652000 <94160086+duy652000@users.noreply.github.com>
2023-01-09 22:41:35 +0700

    hover active 10-1

--- quanlikhachsan/src/pages/Bill.js
+++ quanlikhachsan/src/pages/Bill.js
@@ -71,18 +71,7 @@ function Bill() {
         <div>
           <form className="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search ">
             <div className="input-group">
-              <input
-                type="text"
-                className="form-control border-0 small"
-                placeholder="Tìm kiếm ......"
-                aria-label="Search"
-                aria-describedby="basic-addon2"
-              />
-              <div className="input-group-append">
-                <button className="btn btn-primary" type="button">
-                  <i className="fas fa-search fa-sm"></i>
-                </button>
-              </div>
+             
             </div>
           </form>
         </div>
